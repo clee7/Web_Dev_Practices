092af529a92163e8c1a4d1a8e8a4d8656cdfcd49
Chih-Chieh Lee <clee7@caltech.edu>
2017-03-24 02:39:03 -0700

    Simple Javascript

---
+++ simpleJava.js
@@ -0,0 +1,11 @@
+var firstName = prompt("Please enter your first name: ");
+var lastName = prompt("and your last name: ");
+var age = prompt("What is your age: ");
+var fullName = firstName + " " + lastName;
+
+var days = age * 365.25;
+
+alert(age + " years is roughly " + days + " days");
+
+console.log("Name: " + fullName);
+console.log("Age: " + age);
